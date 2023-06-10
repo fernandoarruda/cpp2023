794ac7b299401b3e81791a42d85b047c2a057662
Fernando <fernandoarruda1384@gmail.com>
2023-06-10 18:39:58 -0300

    four

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
+    // comment in single line
+    /* multiline 
+    comment */
     std::cout << "I like this program" << std::endl;
     std::cout <<"Second line of code";
 
