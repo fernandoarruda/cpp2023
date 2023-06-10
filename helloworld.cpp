c089036b40db0e33d9e7457d6e217a86e1300dee
Fernando <fernandoarruda1384@gmail.com>
2023-06-10 18:36:37 -0300

    third

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout <<"I like this program";
+    std::cout << "I like this program" << std::endl;
+    std::cout <<"Second line of code";
 
     return 0;
 }
